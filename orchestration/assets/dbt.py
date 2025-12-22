3bdb5e86985cb2f5ca4e4168c03d6cf79f8588e4
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-22 19:50:46 +1000

    add dbt dps command to config

--- orchestration/assets/dbt.py
+++ orchestration/assets/dbt.py
@@ -5,7 +5,7 @@ from dagster import AssetExecutionContext, AutomationCondition
 
 # configure dbt project resource
 dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "dbt", "warehouse").resolve()
-dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir), global_args=["deps"])
+dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
 # generate manifest
 dbt_manifest_path = (
