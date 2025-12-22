ede08357819b90aa558607e4a8fbb6b0345e23c7
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-22 19:02:14 +1000

    ad dagster source linking

--- orchestration/assets/dbt.py
+++ orchestration/assets/dbt.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
-from dagster_dbt import DbtCliResource, dbt_assets
-from dagster import AssetExecutionContext
+from dagster_dbt import DbtCliResource, dbt_assets, DagsterDbtTranslator
+from dagster import AssetExecutionContext, AutomationCondition
 
 # configure dbt project resource
 dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "dbt", "warehouse").resolve()
@@ -17,7 +17,11 @@ dbt_manifest_path = (
     .target_path.joinpath("manifest.json")
 )
 
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_automation_condition(self, dbt_resource_props): 
+        return AutomationCondition.eager()
+
 # load manifest to produce asset defintion
-@dbt_assets(manifest=dbt_manifest_path)
+@dbt_assets(manifest=dbt_manifest_path, dagster_dbt_translator=CustomDagsterDbtTranslator())
 def dbt_warehouse(context: AssetExecutionContext, dbt_warehouse_resource: DbtCliResource):
     yield from dbt_warehouse_resource.cli(["run"], context=context).stream()
