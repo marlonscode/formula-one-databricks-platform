45fb3521f2197ba21d352c4d9223b7ad88824656
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-22 13:05:41 +1000

    add schedule to dagster cloud

--- orchestration/assets/airbyte.py
+++ orchestration/assets/airbyte.py
@@ -1,14 +1,25 @@
-from dagster_airbyte import AirbyteWorkspace, build_airbyte_assets_definitions
-import dagster as dg
+from dagster import EnvVar, AutomationCondition, AssetSpec
+from dagster_airbyte import AirbyteWorkspace, build_airbyte_assets_definitions, DagsterAirbyteTranslator, AirbyteConnectionTableProps
+
+class CustomDagsterAirbyteTranslator(DagsterAirbyteTranslator):
+    def get_asset_spec(self, props: AirbyteConnectionTableProps) -> AssetSpec:
+        default_spec = super().get_asset_spec(props)
+        return default_spec.replace_attributes(
+            group_name="airbyte_assets",
+            automation_condition=AutomationCondition.on_cron(cron_schedule="* * * * *")
+        )
 
 # Connect to your OSS Airbyte instance
 airbyte_workspace = AirbyteWorkspace(
     rest_api_base_url="http://ec2-54-206-100-192.ap-southeast-2.compute.amazonaws.com:8000/api/public/v1",
     configuration_api_base_url="http://ec2-54-206-100-192.ap-southeast-2.compute.amazonaws.com:8000/api/v1",
-    workspace_id=dg.EnvVar("AIRBYTE_WORKSPACE_ID"),
-    client_id=dg.EnvVar("AIRBYTE_CLIENT_ID"),
-    client_secret=dg.EnvVar("AIRBYTE_CLIENT_SECRET"),
+    # workspace_id=EnvVar("AIRBYTE_WORKSPACE_ID"),
+    # client_id=EnvVar("AIRBYTE_CLIENT_ID"),
+    # client_secret=EnvVar("AIRBYTE_CLIENT_SECRET"),
 )
 
 # Load all assets from your Airbyte workspace
-airbyte_assets = build_airbyte_assets_definitions(workspace=airbyte_workspace)
+airbyte_assets = build_airbyte_assets_definitions(
+    workspace=airbyte_workspace,
+    dagster_airbyte_translator=CustomDagsterAirbyteTranslator()
+)
