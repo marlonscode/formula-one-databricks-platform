92b84b73cb643ba7ee6c938dbd7b0ea436404135
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-23 17:11:48 +1000

    refactor cicd

---
+++ orchestration/tests/test_dagster_airbyte.py
@@ -0,0 +1,7 @@
+from orchestration.assets.airbyte import connections_list
+
+
+def test_connections_list_not_empty():
+    actual = len(connections_list) > 0
+    expected = True
+    assert actual == expected
