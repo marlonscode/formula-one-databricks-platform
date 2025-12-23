088a4987efc53e948f5d7f0f4e5a309368b6258c
Marlon Moreira <marlon.moreira.runner@gmail.com>
2025-12-23 18:35:20 +1000

    add dagster python test

--- orchestration/tests/test_dagster_airbyte.py
+++ orchestration/tests/test_dagster_airbyte.py
@@ -3,10 +3,6 @@ from orchestration.assets.airbyte import connections_list
 
 
 def test_connections_list_not_empty():
-    # Set dummy environment variables for testing
-    os.environ["AIRBYTE_WORKSPACE_ID"] = "dummy_workspace"
-    os.environ["AIRBYTE_CLIENT_ID"] = "dummy_client"
-    os.environ["AIRBYTE_CLIENT_SECRET"] = "dummy_secret"
     actual = len(connections_list) > 0
     expected = True
     assert actual == expected
